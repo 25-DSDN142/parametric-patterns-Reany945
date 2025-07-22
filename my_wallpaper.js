d7559e32a84bc395aa378f9b7b93a176df6a5f3e
Reany945 <Reany050824@outlook.com>
2025-07-22 12:02:48 +1200

    start wallpaper

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,7 +2,6 @@
 let rect_width  = 20;
 let rect_height = 30;
 
-
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   //pWallpaper.output_mode(GRID_WALLPAPER);
@@ -14,12 +13,37 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
   pWallpaper.grid_settings.row_offset  = 50;
+
+  angleMode(RADIANS);
 }
 
 function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+function my_symbol() {
+  push();
+  translate(100, 100); 
+
+  let numPetals = 5;
+  
+  let angle = TWO_PI / numPetals;
+
+  // 花瓣
+  fill(255, 215, 0);// 黄色
+  
+  noStroke();
+  for (let i = 0; i <= numPetals; i++) {
+    push();
+    rotate(i * angle);           // 每个花瓣旋转到不同角度
+    ellipse(40, 0, 60, 28);      // 花瓣离花心40像素，横向椭圆
+    pop();
+  }
+
+
+  // 花心
+  fill(139, 69, 19);
+  ellipse(0, 0, 32, 32); // 花心
+
+  pop();
 }
