49570d83b11ffdb4d542861a3a6e92c0b520f07a
Reany945 <Reany050824@outlook.com>
2025-07-25 18:38:24 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,49 +1,94 @@
-//your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
+let characterSize = 80;
+let characterStrokeWeight = 2;
 
-function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
-  
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+let patternDensity = 1.0;
 
-  //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+let characterBodyHue = 220;
+let characterBodySaturation = 70;
+let characterBodyBrightness = 80;
 
-  angleMode(RADIANS);
+let eyeHue = 0;
+let eyeSaturation = 0;
+let eyeBrightness = 100;
+
+let mouthHue = 0;
+let mouthSaturation = 100;
+let mouthBrightness = 100;
+
+let backgroundHue = 200;
+let backgroundSaturation = 80;
+let backgroundBrightness = 70;
+
+let mouthStyle = 0;
+
+let earSizeFactor = 0.5;
+
+
+function setup() {
+    createCanvas(2000, 1000);
+    colorMode(HSB, 360, 100, 100);
+    noStroke();
+    noLoop();
 }
 
-function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+function draw() {
+    background(backgroundHue, backgroundSaturation, backgroundBrightness);
+
+    stroke(characterBodyHue, characterBodySaturation, characterBodyBrightness * 0.7);
+    strokeWeight(characterStrokeWeight);
+
+    let tileWidth = characterSize * patternDensity * 2.5;
+    let tileHeight = characterSize * patternDensity * 2.5;
+
+    for (let x = 0; x < width; x += tileWidth) {
+        for (let y = 0; y < height; y += tileHeight) {
+            push();
+
+            translate(x + tileWidth / 2, y + tileHeight / 2);
+
+            my_symbol(characterSize);
+
+            pop();
+        }
+    }
 }
 
-function my_symbol() {
-  push();
-  translate(100, 100); 
 
-  let numPetals = 5;
-  
-  let angle = TWO_PI / numPetals;
+function my_symbol(size) {
+    let scaleFactor = size / 100;
+    scale(scaleFactor);
 
-  // 花瓣
-  fill(255, 215, 0);// 黄色
-  
-  noStroke();
-  for (let i = 0; i <= numPetals; i++) {
-    push();
-    rotate(i * angle);           // 每个花瓣旋转到不同角度
-    ellipse(40, 0, 60, 28);      // 花瓣离花心40像素，横向椭圆
-    pop();
-  }
+    fill(characterBodyHue, characterBodySaturation, characterBodyBrightness);
+    ellipse(0, 0, 120, 60);
 
+    fill(characterBodyHue, characterBodySaturation, characterBodyBrightness);
+    triangle(60, 0, 100, -40, 90, 0);
+    triangle(60, 0, 90, 20, 90, 0);
 
-  // 花心
-  fill(139, 69, 19);
-  ellipse(0, 0, 32, 32); // 花心
+    fill(characterBodyHue, characterBodySaturation, characterBodyBrightness);
+    triangle(-10, 30 * earSizeFactor, -22, 50 * earSizeFactor, 2, 50 * earSizeFactor);
 
-  pop();
+    fill(characterBodyHue, characterBodySaturation, characterBodyBrightness);
+    triangle(-60, 0, -100, -40 * earSizeFactor, -90, 0);
+    triangle(-60, 0, -90, 20 * earSizeFactor, -90, 0);
+
+    fill(mouthHue, mouthSaturation, mouthBrightness);
+    if (mouthStyle === 0) {
+        arc(-20, 0, 20, 20, 0, PI, CHORD);
+    } else if (mouthStyle === 1) {
+        rectMode(CENTER);
+        rect(-20, 0, 15, 5);
+    } else {
+        arc(-20, 0, 20, 20, 0, PI, CHORD);
+    }
+
+    fill(eyeHue, eyeSaturation, eyeBrightness);
+    ellipse(20, -20, 20, 20);
+    fill(0);
+    ellipse(20, -20, 10, 10);
+
+    fill(eyeHue, eyeSaturation, eyeBrightness);
+    ellipse(-35, -15, 15, 15);
+    fill(0);
+    ellipse(-35, -15, 6, 6);
 }
