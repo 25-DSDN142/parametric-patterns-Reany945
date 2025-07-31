12c2165d28a34cdfe580ef39372254da32df83c3
Reany945 <Reany050824@outlook.com>
2025-08-01 02:06:29 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,28 +1,28 @@
-let Role = 60; // Size of the character
-let lineThickness = 1.0; // Thickness of the lines
+let Role = 100; // Size of the character
+let lineThickness = 3.0; // Thickness of the lines
         
-let mainColorR = 200;   
-let mainColorG = 150;
-let mainColorB = 255;
+let mainColorR = 255;   
+let mainColorG = 200;
+let mainColorB = 150;
 
-let densityFactor = 0.7; // Density of the pattern, 0.7 for medium density
+let densityFactor = 1; // Density of the pattern, 0.7 for medium density
 
 let eyeColorR = 255;
 let eyeColorG = 255;
 let eyeColorB = 255;
 
-let mouthColorR = 0;
-let mouthColorG = 200;
+let mouthColorR = 255;
+let mouthColorG = 255;
 let mouthColorB = 0;
 
 let backgroundColorR = 50;
 let backgroundColorG = 76;
 let backgroundColorB = 168;
 
-let characterStyle = 0; // 0 for arc mouth, 1 for rectangle mouth, 
+let characterStyle = 1; // 0 for arc mouth, 1 for rectangle mouth, 
 
-let earScaleFactor = 0.8;
-let eyeSizeFactor = 1.5;
+let earScaleFactor = 1.2;
+let eyeSizeFactor = 2.5;
 
 function setup() {
 createCanvas(2000, 1000);
